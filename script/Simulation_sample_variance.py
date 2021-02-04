7eec6833277f596a57fa763b9656d91e0071ceec
Felix M <felix.w.mir@gmail.com>
2021-02-04 09:35:55 +0100

    cleaned up. prints, comments, naming, labels

--- script/Simulation_sample_variance.py
+++ script/Simulation_sample_variance.py
@@ -1,22 +1,18 @@
-import pandas as pd
+#import pandas as pd
 import numpy as np
-from collections import Counter
+#from collections import Counter
 import matplotlib.pyplot as plt
 import random
-import time
+# import time
 
-# print('hello')
 
-##### todo ###
-# create random creation of distribution calc N, mean, variance of population
-# take samples of si
+
 
 
 ### POPULATION - compute N, mean, var
-N = 600
+N = 600                 #Populaltion size
 pop_list = []
-pop_average = None
-
+pop_average = None      #Population average
 number_of_bars =6
 
 prob_pop_distr=[]
@@ -32,92 +28,84 @@ pop_array = np.array(pop_list)
 
 pop_variance = np.var(pop_array)
 pop_average = np.mean(pop_array)
-print("N","pop avg"," pop variance" )
-print(N,round(pop_average,2), round(pop_variance,2))
 
-## count occurence, prepare for bar chart
+
+# Count occurences (number of y-element) for every x-element
 x_nr=[]
 y_nr=[]
 for i in range(1,7):
     x_nr.append(str(i))
     y_nr.append(pop_list.count(i))
 
-# print(x_nr,y_nr)
 
-### graph - bar chart population
+
+# Graph - bar chart - population
 fig, axs = plt.subplots(3, 1)
 axs[0].bar(x_nr,y_nr)
-axs[0].set_title('population distr')
-
-# fig1 = plt.figure(1)
-# ax = fig1.add_axes([0,0,1,1])
-# ax.bar(x_nr,y_nr)
+axs[0].set_title('Population distribution \n N='+str(N)+', mean='+str(pop_average)+', variance='+str(pop_variance))
+axs[0].set_ylabel('Counts')
+axs[0].set_xlabel('Value of random variable')
 
 
-#### population probabilities
+# Population probabilities
 tot_y = sum(y_nr)
 prob_pop=[]
 for i in y_nr:
     prob_pop.append(i/tot_y)
 
-# print(prob_pop)
 sample_var_list=[]
 sample_avg_list=[]
 sample_size_list=[]
 
 
-for ii in range(100):
-    sample_size= random.randrange(2,11) # sample sizes
+# Sample from the distribution
+number_of_samples= 100
+max_sample_size=10
+
+for ii in range(number_of_samples):
+    sample_size= random.randrange(2,max_sample_size+1) # sample sizes randomized
     sample_list = []
-    sample_average = None
+
     for i in range(sample_size):
         sample = np.random.choice(np.arange(1, number_of_bars+1), p=prob_pop)
         sample_list.append(sample)
 
     sample_array = np.array(sample_list)
-    sample_variance = np.var(sample_array)
-    sample_average = np.mean(sample_array)
 
-    sample_var_list.append(sample_variance)
-    sample_avg_list.append(sample_average)
-    sample_size_list.append(sample_size)
-
-# print(sample_var_list)
-# print(sample_avg_list)
-# print(sample_size_list)
+    sample_var_list.append(np.var(sample_array))    # sample variance
+    sample_avg_list.append(np.mean(sample_array))   # sample average
+    sample_size_list.append(sample_size)            # sample size
 
 
-    ### graph - scatter - sample variance
+    # Graph - scatter plot - sample variance vs mean
     axs[1].scatter(sample_avg_list, sample_var_list)
-    axs[1].set_title('sample variance vs average')
-    axs[1].axhline(y=pop_variance, color='r', linestyle='-')
-    axs[1].axvline(x=pop_average, color='r', linestyle='-')
-    plt.pause(0.1)
+    axs[1].set_title('Sample statistics - Lines are Pop. param.')
+    axs[1].axhline(y=pop_variance, color='r', linestyle='-') # population variance
+    axs[1].axvline(x=pop_average, color='r', linestyle='-')  # population mean
+    axs[1].set_ylabel('Biased Sample Variance')  # sum( (x(i) - x_mean)^2) / n
+    axs[1].set_xlabel('Sample mean')
 
-    ### compute sample size vs variance
+    plt.pause(0.01)
 
-    sample_avg_var = []
+    # Compute sample var / pop variance for every sample size
+    avg_sample_var_vs_pop_var = []
     size_nr = []
-    for i in range(2, 11):
+    for s_size in range(2, max_sample_size+1): # sample sizes
         sample_var_nr_renew=[]
-        for a,size in enumerate(sample_size_list):
-            if size == i:
-                print(a,size, sample_var_list[a])
-                sample_var_nr_renew.append(sample_var_list[a])
-        if len(sample_var_nr_renew)==0:
-            avg_var=0
+        for a_nr,size in enumerate(sample_size_list):
+            if size == s_size:
+                sample_var_nr_renew.append(sample_var_list[a_nr])
+        if len(sample_var_nr_renew)==0: # no samples for one sample size
+            avg_sample_var=0
         else:
-            avg_var=np.mean(sample_var_nr_renew)
-        sample_avg_var.append(avg_var/pop_variance)
-        size_nr.append(i)
-    # print(size_nr)
-    # print(sample_avg_var)
+            avg_sample_var=np.mean(sample_var_nr_renew)
+        avg_sample_var_vs_pop_var.append(avg_sample_var/pop_variance)
+        size_nr.append(s_size)
 
-
-    ### graph - sample size vs variance
-    axs[2].bar(size_nr,sample_avg_var)
-    axs[2].set_title('sample size vs variance')
-
-
+    # Graph - Sample size vs variance
+    axs[2].bar(size_nr,avg_sample_var_vs_pop_var)
+    axs[2].set_title('Sample Size vs. Variance \n'+ str(number_of_samples)+' samples')
+    axs[2].set_ylabel('Biased Sample Var/Pop. var (%)')
+    axs[2].set_xlabel('Sample mean')
 
 plt.show()
